a37944a257d9084ebe4bc798883a3f2d8dba23a2
macwray17 <55639501+macwray17@users.noreply.github.com>
2023-03-15 12:07:02 -0700

    Managing State

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+/** @format */
+
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>;
+  return (
+    <div>
+      <ListGroup />
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,13 @@
 /** @format */
 
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["Claremont", "Upland", "Montclair", "Pasadena", "Los Angeles"];
+
+  //Hook: Allows for built in features in React
+  //Need because we need to put a selectedIndex variable into our react element
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const emptyMessage = items.length === 0 ? <p>No Item Found</p> : null;
 
@@ -11,16 +15,23 @@ function ListGroup() {
     return items.length === 0 && <p>No Item Found</p>;
   };
 
-  //Event Handler
-  const handleClick = (event: MouseEvent) => console.log(event);
-
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
