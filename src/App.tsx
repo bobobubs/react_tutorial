e3eed1f0276831817d9b8895f313d9d25a9dc578
macwray17 <55639501+macwray17@users.noreply.github.com>
2023-08-28 22:16:05 -0500

    Effect Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,24 @@
 
 import { useState, useRef, useEffect } from "react";
 import "./index.css";
+import ProductList from "./components/ProductList";
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
+  const [category, setCategory] = useState("");
 
-  //function should really be called afterRender
-  useEffect(() => {
-    //changes the focus of the DOM element to be the ref if ref.current is defined
-    //has a "side effect" ie no longer a pure component.
-    //can make it a pure component by using a ref hook.
-    if (ref.current) ref.current.focus();
-  });
-
-  //effect that changes the title of the document.
-  useEffect(() => {
-    document.title = "My App";
-  });
+  // afterRender
+  useEffect(() => {});
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </div>
   );
 }
