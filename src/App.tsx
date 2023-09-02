ae6f3fbc322683e8f21c52d6c206902ed1fafc30
Mason Wray <macwray17@gmail.com>
2023-09-02 18:44:39 -0500

    Creating Data

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,23 @@ function App() {
 
     setUsers(users.filter(u => u.id !== user.id));
 
-    axios.delete("https://jsonplaceholder.typicode.com/ users/" + user.id).catch((error) => {
+    axios.delete("https://jsonplaceholder.typicode.com/users/" + user.id).catch((error) => {
+      setError(error.message);
+      setUsers(originalUsers);
+    });
+  }
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    //optimistic paradigm so update the UI 
+    const newUser = {id: 0,  name: "Mason"};
+    setUsers([newUser, ...users]);
+
+    //then update the data on the server.
+    //use the axios.post() method to post new information to the server
+    axios.post("https://jsonplaceholder.typicode.com/users/", newUser).then
+    //on this line destructuring the response and using an alias to make the code easier to read.
+    (({data: savedUser}) => setUsers([savedUser, ...users])).catch((error) => { 
       setError(error.message);
       setUsers(originalUsers);
     });
@@ -48,6 +64,8 @@ function App() {
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
+
+      <button  className="btn btn-primary mb-3" onClick={addUser}>Add</button>
       <ul className="list-group">
         {users.map((user) => (
           <li key={user.id} className="list-group-item  d-flex justify-content-between">{user.name} <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button></li>
