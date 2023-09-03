7519679ccb0f18875c535bbb9180cf9e18b947fd
Mason Wray <macwray17@gmail.com>
2023-09-02 23:17:38 -0500

    Creating a Custom Data Fetching Hook

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import userService, { User } from "../services/user-service";
+import { CanceledError } from "../services/api-client";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //the effect hook now has no knowledge of http requests thus better separtion of concerns
+  useEffect(() => {
+    setIsLoading(true);
+    const { request, cancel } = userService.getAll<User>();
+    request
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => cancel();
+  }, []);
+
+  //return all the state variables of your hook
+  //also need to return required set functions
+  return { users, error, isLoading, setUsers, setError };
+};
+
+export default useUsers;
